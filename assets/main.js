880eb29e21899e029ab2a60e04774ab3a77f8761
Marlon Silva do Prado <marlon-prado@SEPLAG.INTRA.RS.GOV.BR>
2023-11-21 15:31:14 -0300

    feat: add all functions

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,9 @@ const display = document.querySelector(".calculator__display");
 const numberKeys = document.querySelectorAll(".calculator__button.num");
 const functionKeys = document.querySelectorAll(".calculator__button.fun");
 let displayLog = 0;
-let operation = "null";
+let operation = null;
+let number1 = 0; // Variável para o primeiro número da operação
+let number2 = 0; // Variável para o segundo número da operação
 
 
 // Função para zerar o display
@@ -22,37 +24,95 @@ function resetDisplayLog() {
   displayLog = 0;
 }
 
-function calculator(clickedOperation){
-  
-  // Switch para realizar operação de acordo com o clique
-  switch (clickedOperation) {
-    // No caso de ser clicada o botão C, reseta valores
+// Função principal que será chamada quando uma tecla for clicada
+function calculator(clickedKey) {
+  switch (clickedKey) {
+    // No caso da tecla pressionada ser a C, faz reset dos valores
     case "key__c":
       resetDisplay();
       resetDisplayLog();
+      operation = null;
+      number1 = 0;
+      number2 = 0;
       break;
-    // No caso de clicar em "soma"
+    case "key__plus_minus":
+      if (Number(display.textContent) !== 0) {
+        display.textContent = (Number(display.textContent) * -1)
+      }
+      break;
+    case "key__backspace":
+      if (Number(display.textContent) !== 0 & display.textContent.length > 1) {
+        display.textContent = display.textContent.slice(0, -1);
+        console.log("if backspace")
+      }else{
+        display.textContent = "0"
+        console.log("else backspace")
+      }
+      break;
+    // No caso de ser qualquer uma das demais operações...
     case "key__sum":
-      if(number1 === 0){
-        number1 = number2;
-        console.log(number1)
-        console.log(displayLog)
-      }else if (number2 )
-      
+    case "key__subtract":
+    case "key__multiply":
+    case "key__divide":
+    case "key__potention":
+      // Verifica se uma operação já está em andamento e realiza a operação pendente
+      if (operation !== null) {
+        realizeOperation();
+      }
+      // Senão, armazena o valor digitado na variável 1
+      number1 = parseFloat(display.textContent);
+      operation = clickedKey;
+      resetDisplay();
       break;
     case "key__equals":
-
+      if (operation !== null) {
+        realizeOperation();
+      }
       break;
-}
-
-
-// Função para realizar a operação de acordo com tecla clicada
-function realizeOperation(clickedOperation) {
-  // Switch para selecionar operação de acordo
- 
+    default:
+      break;
   }
 }
 
+// Função para realizar a operação de acordo com operação armazenada em memória
+function realizeOperation() {
+  // Escolhe a partir da variável "operation" de acordo com a operação
+  switch (operation) {
+    case "key__sum":
+      number2 = parseFloat(display.textContent);
+      displayLog = number1 + number2;
+      display.textContent = displayLog.toString();
+      break;
+    case "key__subtract":
+      number2 = parseFloat(display.textContent);
+      displayLog = number1 - number2;
+      display.textContent = displayLog.toString();
+      break;
+    case "key__multiply":
+      number2 = parseFloat(display.textContent);
+      displayLog = number1 * number2;
+      display.textContent = displayLog.toString();
+      break;
+    case "key__divide":
+      number2 = parseFloat(display.textContent);
+      if (number2 !== 0) {
+        displayLog = number1 / number2;
+        display.textContent = displayLog.toString();
+      } else {
+        displayLog = "Error";  // Tratamento de divisão por zero
+        display.textContent = displayLog.toString();
+      }
+      break;
+    case "key__potention":
+      number2 = parseFloat(display.textContent);
+      displayLog = number1 ** number2;
+      display.textContent = displayLog.toString();
+      break;
+    default:
+      break;
+  }
+
+}
 // Varrendo teclas de função em busca de cliques
 functionKeys.forEach((key) => {
   // Adicionando verificação de click para cada tecla
@@ -70,8 +130,10 @@ numberKeys.forEach((key) => {
     if (Number(display.textContent) === 0) {
       // Se sim, passando valor do botão clicado no momento
       display.textContent = key.textContent;
-    } else {
-      // Se não, concatenando o valor do botão clicado ao lado do valor já existente
+    }
+    // Senão, verifica se o número é menor que o limite do display
+    else if (display.textContent.length < 19) {
+      // Concatenando o valor do botão clicado ao lado do valor já existente
       display.textContent += key.textContent;
     }
   });
